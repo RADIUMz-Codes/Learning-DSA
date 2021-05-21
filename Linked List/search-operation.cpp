966842acf686eda6dff99f26dd22a2ca30dc804d
RADIUMz-Codes <69766210+theabhikr03@users.noreply.github.com>
2021-05-22 02:33:45 +0530

    search

---
+++ Linked List/search-operation.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head,int d)
+{
+    node*n= new node(d); 
+    if(head== NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node*temp= head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=n;
+
+}
+
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data;
+        cout << "->";
+        head = head->next;
+    }
+}
+
+//search takes O(n) time in worst case
+int search(node *head,int key){
+    if(head==NULL)
+    {
+        return -1;
+    }
+    int p=1;
+    while(head->next!= NULL)
+    {
+        if(head->data==key )
+        {
+            return p;
+        }
+        p++;head= head->next;
+    }
+}
+
+
+bool searchRecursive(node *head,int key)
+{
+    if(head->data==key)
+    return true;
+
+    if(head->next == NULL)
+    return false;
+
+    searchRecursive(head->next,key);
+
+}
+
+
+int main()
+{
+    node*head=NULL;
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+
+    cout<<search(head,1)<<endl;
+    cout<<searchRecursive(head,1)<<endl;
+    cout <<searchRecursive(head,20);
+
+    // print(head);
+    return 0;
+}
