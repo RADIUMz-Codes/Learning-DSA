9060dab622c9b0006e07e792852f2e8dda83e57b
RADIUMz-Codes <69766210+theabhikr03@users.noreply.github.com>
2021-05-21 12:03:18 +0530

    ignore

--- Sorting Challanges/bin-sort.cpp
+++ Sorting Challanges/bin-sort.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 int main()
 {
-    
+    long long n;
+    cin>>n; 
+    vector<long long> v(n);
+    for(long long i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    long long m;
+    cin>>m;
+
+    sort(v.begin(),v.end());
+
+    if(binary_search(v.begin(),v.end(),m))
+    {
+        auto it =lower_bound(v.begin(),v.end(),m);
+        cout<<(it-v.begin())<<endl;
+    }
+    else
+    {
+        cout<<-1;
+    }
     return 0;
 }
