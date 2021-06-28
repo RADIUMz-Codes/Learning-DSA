0badd5770ac01a45a8ed4b98c544a9e03c8a341d
RADIUMz-Codes <69766210+theabhikr03@users.noreply.github.com>
2021-06-28 21:36:50 +0530

    Push

---
+++ Recursion-Challanges/Friends-Pairing.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fn(int n){
+	if(n<=1)
+	{
+		return 1;
+	}
+	if(n<0)
+	{
+		return 0;
+	}
+
+	int alone=fn(n-1);
+	int dbl=(n-1)*fn(n-2);
+	return alone+dbl;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	while(n!=-1)
+	{
+    	cout<<fn(n)<<endl;
+    	cin>>n;
+	}
+}
