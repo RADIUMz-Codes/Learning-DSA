bcd082c3fef46d7f1b0b8b2c2ed487acd5cbd4f6
RADIUMz-Codes <theabhikr03@gmail.com>
2022-06-12 03:57:57 +0530

    changed Graph

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -5,6 +5,7 @@ template <typename T>
 class Graph
 {
     map<T, list<T>> l;
+
 public:
     void addEdge(int x, int y)
     {
@@ -20,9 +21,9 @@ public:
         visited[src] = true;
         while (!q.empty())
         {
-            T node = q.front(); 
+            T node = q.front();
             q.pop();
-            cout<<node<<" -> ";
+            cout << node << " -> ";
             for (auto nbr : l[node])
             {
                 if (!visited[nbr])
@@ -39,10 +40,10 @@ int main()
 {
     Graph<int> g;
     g.addEdge(0, 1);
-    g.addEdge(3, 4);
-    g.addEdge(1, 5);
+    g.addEdge(0, 2);
     g.addEdge(2, 3);
-    g.addEdge(4, 5);
+    g.addEdge(2, 4);
+    g.addEdge(3, 4);
     g.bfs(0);
     return 0;
 }
